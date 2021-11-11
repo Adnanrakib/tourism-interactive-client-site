a77631f0a10cc1c5d8e37901b9def00fc96b1d57
Adnanrakib <adnanrakib550@gmail.com>
2021-11-12 02:30:06 +0600

    update code site

--- src/pages/PrivateRoute/Myorders.js
+++ src/pages/PrivateRoute/Myorders.js
@@ -4,6 +4,7 @@ const Myorders = () => {
           return (
                     <div>
                               <h1>My All Orders</h1>
+                              <h1>Booking now</h1>
                     </div>
           );
 };
